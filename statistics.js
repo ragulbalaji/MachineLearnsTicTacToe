cf9069204c28bcbae4aa94f90f64ce47aaf051b5
iamromuald <github@committers.org>
2016-10-06 14:45:15 +0200

    refactor(server): Clean server.js code
    Refactor neural code to neural.js
    Refactor statistics.js to expose loadStats method. WIP.

--- statistics.js
+++ statistics.js
@@ -1,4 +1,5 @@
 var fs = require('fs');
+
 var stats = function(){
   this.wins = 0;
   this.loses = 0;
@@ -19,15 +20,22 @@ var stats = function(){
     saveStats();
   }
 };
+
 function saveStats() {
   fs.writeFile("data/stats.json",JSON.stringify(stats));
-};
-fs.readFile("data/stats.json",function(err,data){
-  if(err)
-  {
-    console.error(err);
-  }
-  stats = JSON.parse(data);
-});
+}
+
+function loadStats() {
+  fs.readFile("data/stats.json",function(err,data){
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log("LOADED FROM data/stats.json >> " + data);
+    stats = JSON.parse(data);
+  });
+}
+
 module.exports.stats = stats;
-module.exports.saveStats=saveStats;
+module.exports.saveStats = saveStats;
+module.exports.loadStats = loadStats;
