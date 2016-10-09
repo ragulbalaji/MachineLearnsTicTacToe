6af783b2e888b5e2456bcb449d28ed7543fc3ae9
Kaelan Mikowicz <eternity01033@gmail.com>
2016-10-09 02:41:27 -0700

    Fixed errors made in pull request #4. (#16)
    * Update .travis.yml
    
    * Missing module.exports
    
    Also made sure the load functions are called within each module after
    definition

---
+++ app/tests/UnitTest.js
@@ -0,0 +1,16 @@
+﻿var assert = require('assert');
+var fs = require('fs');
+var neural = require('../neural.js');
+describe('Neural Network', function() {
+    it('loadTrainingData', function () {
+        fs.createReadStream("../../data/trainingdata.json");
+
+        neural.loadTrainingData();
+        assert.equals
+    })
+
+    it('Test 2', function() {
+        assert.ok(1 === 1, "This shouldn't fail");
+        assert.ok(false, "This should fail");
+    })
+})
---
+++ typings/index.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="globals/body-parser/index.d.ts" />
+/// <reference path="globals/express/index.d.ts" />
+/// <reference path="globals/mocha/index.d.ts" />
+/// <reference path="globals/node/index.d.ts" />
+/// <reference path="globals/string/index.d.ts" />
